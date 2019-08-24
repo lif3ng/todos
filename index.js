ece745c24548906d2ba3547e1cefdb102b5fa033
lif3ng <lif3ng@icloud.com>
2019-08-24 11:32:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,16 @@ function render(div, query) {
   loadData(url)
     .then((issues) => {
 //       console.log({ issues })
-      let itemsStr = issues.map(({ title, url, labels }) => {
+      let itemsStr = issues.map(({ title, url, html_url, labels, created_at, closed_at }) => {
         let labelStr = labels.map(({ name, color }) =>
           `<span class="label" style="border:1px solid #${color}">${name}</span>`
         ).join('')
+        const timeStr = query.state === 'closed' ? closed_at : create_at;
         return `<div class="issue">
-    <a href="${url}">${title}</a>
-    <div class="labels">
+    <a href="${html_url}" traget="_blank">${title}</a>
+    <span class="labels">
         ${labelStr}
-    </div>
+    </span><span class="time">${new Date(timeStr).toLocaleString()}</span>
 </div>`
       }).join('')
       div.innerHTML = itemsStr
@@ -39,5 +40,5 @@ function render(div, query) {
 
 render(document.getElementById('new'), {per_page:5})
 
-render(document.getElementById('done'), {per_page:5,sort:'update'})
+render(document.getElementById('done'), {per_page:5,state:'closed'})
 
